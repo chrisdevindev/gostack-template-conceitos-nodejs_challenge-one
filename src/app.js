2f915216f2cba827d86fba657260d42df50818d4
Christian <alvescastro00cca13@gmail.com>
2020-09-21 15:50:58 -0300

    Challenge init

--- src/app.js
+++ src/app.js
@@ -1,21 +1,33 @@
 const express = require("express");
 const cors = require("cors");
 
-// const { v4: uuid } = require('uuid');
+const { v4: uuid } = require('uuid');
 
 const app = express();
 
 app.use(express.json());
 app.use(cors());
 
+
+
+
+
+
+const repositories = [];
 const repositories = [];
 
 app.get("/repositories", (request, response) => {
   // TODO
+  console.log("deu certo")
 });
 
 app.post("/repositories", (request, response) => {
-  // TODO
+  const { title, url, techs, likes } = request.body
+
+  const repositorie = {id: uuid(), title, url, techs, likes}
+
+  repositories.push(repositorie)
+  return(response.json(repositorie))
 });
 
 app.put("/repositories/:id", (request, response) => {
